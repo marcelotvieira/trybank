cf4e466b8471a2148673a77838be5bbcc7ee1f60
marcelotvieira <marcelodefy15@gmail.com>
2023-10-25 09:49:14 -0300

    Feat - Requsito 4
    Construído método CheckBalance para checar o saldo

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -106,10 +106,11 @@ public class TrybankLib
         loggedUser = -99;
     }
 
-    // 4. Construa a funcionalidade de checar o saldo
     public int CheckBalance()
     {
-        throw new NotImplementedException();
+        if (!Logged)
+            throw new AccessViolationException("Usuário não está logado");
+        return Bank[loggedUser, 3];
     }
 
     // 5. Construa a funcionalidade de depositar dinheiro
